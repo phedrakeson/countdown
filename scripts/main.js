8d3f643a6f0484e7e067bee3c01e3c0c943384bb
phedrakeson <gabrield.developer@gmail.com>
2020-12-20 00:13:37 -0300

    adding a simple DOM view

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,11 @@
 import Countdown from './modules/countdown.js';
 
-const daysToChristmas = new Countdown('31 December 2020 23:59:59 GMT-0300');
+const daysToChristmas = new Countdown('24 December 2020 23:59:59 GMT-0300');
 
 setInterval(() => {
   console.log(daysToChristmas.totalTimeLeft);
+  document.querySelector('[data-day]').innerText = daysToChristmas.totalTimeLeft.days;
+  document.querySelector('[data-hours]').innerText = daysToChristmas.totalTimeLeft.hours;
+  document.querySelector('[data-minutes]').innerText = daysToChristmas.totalTimeLeft.minutes;
+  document.querySelector('[data-seconds]').innerText = daysToChristmas.totalTimeLeft.seconds;
 }, 1000)
